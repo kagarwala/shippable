328f29690b4e473f7f450bcf835a7730d118fdd6
kunal <kunal>
2015-09-05 03:02:02 +0530

    'Final'

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# Spring MVC and Hibernate template application
+-Used Spring MVC based app
 
-This is a template for a web application that uses Spring MVC and Hibernate. The sample code is a simple CRUD page that manipulates records for a single model object.
+-For repo issue used githubapi - https://developer.github.com/v3/issues/#list-issues-for-a-repository
 
-## Running the application locally
+- For Ui used bbotstrap and for rest client used spring resttemplate.
 
-First build with:
+- The solution gives issues opened (might be reopened/created) within 24 hours/1week timeframes. NOT in a calender day/week.
 
-    $mvn clean install
+- Used github apis "updated_at" attribute
 
-Then run it with:
 
-    $java -jar target/dependency/webapp-runner.jar target/*.war
+Explanation:-
+It seems github api doesn't expose any statistic api for issue, so we will need to use the detailed list of repo issues api and traverse to build the data.
+We only need updated_at attribute for each issue as the question asks opened within 24 hours.
+
+Improvement:-
+
+- Need more corner case handling
+-As every request, involves many calls to github Api and if we need this info on regular basis, then we can build a git repo issue metadata in our own datastore. Say, we keep updating our issue metadata db for each call, so next time we get a new call we only get the delta from github.
+
 
--- src/main/java/com/example/controller/IssueController.java
+++ src/main/java/com/example/controller/IssueController.java
@@ -40,13 +40,16 @@ public class IssueController {
       return new ModelAndView("issue", "command", new Issue());
    }
    
+   // This method is mapped to add issue button
    @RequestMapping(value = "/track", method = RequestMethod.POST)
    public String trackIssue(@RequestParam("url") String url, 
    ModelMap model) {
 	   try{
       model.addAttribute("url", url);
+      // get the owner and repo from the provided url
       info = getOwnerNRepo( url);
       
+      // build the whole data
       buildData(1);
 	   }catch(Exception e){
 		   
@@ -54,11 +57,20 @@ public class IssueController {
     model.addAttribute("total", total);	
     model.addAttribute("first", first);	
     model.addAttribute("second", second);
+    
+    // once we have the total, issues opened beyond can be calculated indirectly
     model.addAttribute("third", total - first - second);	
       return "track";
    }
    public final static long MILLIS_PER_DAY = 24 * 60 * 60 * 1000L;
    public final static long MILLIS_PER_WEEK = 7 * 24 * 60 * 60 * 1000L;
+   /*
+    * 
+    * This function extracts all the issue updataion time data recursively.
+    * As github api only allows 100 records per page, it calls the api recursivley until all the pages have been encountered.
+    * 
+    * 
+    */
 private void buildData(int page) {
 	// TODO Auto-generated method stub
 	List<GitIssue> resultList = new ArrayList<GitIssue>();
@@ -66,12 +78,18 @@ private void buildData(int page) {
 	try{
 	
 	String gitUrl = String.format("https://api.github.com/repos/%s/%s/issues?state=open&per_page=100&page=%d", info.get("owner"),info.get("repo"),page);
+	
     RestTemplate restTemplate = new RestTemplate();
     List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
   messageConverters.add(new MappingJacksonHttpMessageConverter());
   restTemplate.setMessageConverters(messageConverters);
+  
+  // GitIssue is the model which represents a single issue object with only 'update_at' attribute
+  
    resultList = new ArrayList<GitIssue>();
    resultList = Arrays.asList(restTemplate.getForObject(gitUrl, GitIssue[].class));
+   
+   // this is ISO format in which update_at vaue is returned
    DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
    Date issueDate = null;
    long now = new Date().getTime();
@@ -81,9 +99,13 @@ private void buildData(int page) {
 	   issueDate = df1.parse(g.getUpdated_at());
 	  
 	    diff = now - issueDate.getTime();
+	    
+	    // calculate the ones updated within last 24 hours
 	   if( diff < MILLIS_PER_DAY){
 		   first++;
 	   }else{
+		   
+		   // calculate the ones updated within 24 hours and 7 days
 		   if(diff < MILLIS_PER_WEEK){
 			   second++;
 		   }else{
@@ -99,16 +121,26 @@ private void buildData(int page) {
   	 
   
   }
+	// for every page add up the total records returned in the current page
 	total = total + resultList.size();
 	if(resultList.size() == 100){
 		buildData(page +1);
 	}
 }
 
+/**
+ * This method rtruns a map contaning onwer and repo from the url
+ * 
+ * @param url
+ * @return
+ * @throws Exception
+ */
 private Map<String, String> getOwnerNRepo(String url) throws Exception {
 	// TODO Auto-generated method stub
 	Map<String, String> info = new HashMap<String, String>();
 	String lowerUrl = url.toLowerCase();
+	
+	//Assumption:- Github pubic repo always start with this
 	if(!lowerUrl.startsWith(gitPublicDomain)){
 		throw new InvalidUrlException();
 	}
@@ -116,6 +148,8 @@ private Map<String, String> getOwnerNRepo(String url) throws Exception {
 	String[] parts = lowerUrl.split("/");
 	if(parts.length < 2)
 		throw new InvalidUrlException();
+	
+	// after domain the next 2 part should be owner and repo.
 	info.put("owner", parts[0]);
 	info.put("repo", parts[1]);
 	return info;
